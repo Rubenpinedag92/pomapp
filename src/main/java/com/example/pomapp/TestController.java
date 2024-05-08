ba1400066dbe74c194442cd7e9e3cc0b59bd54a3
Rubenpinedag92 <ruben.pinedag@outlook.com>
2024-05-07 18:42:38 -0600

    Remove param

--- src/main/java/com/example/pomapp/TestController.java
+++ src/main/java/com/example/pomapp/TestController.java
@@ -2,14 +2,14 @@ package com.example.pomapp;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class TestController {
     
-    @GetMapping("hola")
-    public String getMethodName(@RequestParam String param) {
+    @GetMapping("/hola")
+    public String getMethodName() {
+
         return new String("Hola chavo");
     }
     
