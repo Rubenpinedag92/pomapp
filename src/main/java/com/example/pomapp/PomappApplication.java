a58bf480deabee5caa70132bcb023983500f4a34
Rubenpinedag92 <ruben.pinedag@outlook.com>
2024-05-07 18:26:54 -0600

    initial commit

---
+++ src/main/java/com/example/pomapp/PomappApplication.java
@@ -0,0 +1,13 @@
+package com.example.pomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pomapp/PomappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
